8828dd669e73c11e59e3e613e1272795059669a1
jongin1004 <bill1224@naver.com>
2023-07-06 23:30:34 +0900

    websocket 설정 (http위에 websocket 서버를 얹어서 동일한 포트에서 동작하도록)

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
+import { WebSocketServer } from "ws";
+import http from 'http';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { createPublicKey } from "crypto";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,6 +15,22 @@ app.set('views', __dirname + '/views');
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.render('home'));
+app.get('/*', (req, res) => res.redirect('/'));
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000);
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server });
+
+const handleConnection = (socket) => {
+    console.log(socket);
+}
+
+// const handleListening = (data) => {
+//     console.log(data);
+// }
+wss.on('connection', handleConnection);
+
+// wss.on('listening', handleListening);
+
+server.listen(3000, handleListen);
