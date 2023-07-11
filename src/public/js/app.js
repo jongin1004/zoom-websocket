92e83b4c53daac242a452c728489e96800b7c79c
jongin1004 <bill1224@naver.com>
2023-07-11 23:32:38 +0900

    Turn on/off the Audio & video And render the list of vedio devices

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -3,12 +3,31 @@ const socket = io();
 const myFace    = document.querySelector('#myFace');
 const muteBtn   = document.querySelector('#muteBtn');
 const cameraBtn = document.querySelector('#cameraBtn');
+const cameraSelect = document.querySelector('#cameraSelect');
 
 let myStream;
 let muted     = false;
 let cameraOff = false;
 
-async function getMedia() {
+const renderCameraSelect = (videoDevices) => {
+        
+    const option = videoDevices.map(video => `<option value="${video.deviceId}">${video.label}</option>`);
+
+    cameraSelect.insertAdjacentHTML('beforeend', option);
+}
+
+const getCameras = async () => {
+    try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+
+        const videoDevices = devices.filter(device => device.kind === 'videoinput');
+        renderCameraSelect(videoDevices);
+    } catch (e) {
+        console.error(e.message);
+    }
+}
+
+const getMedia = async () => {
     try {
 
         myStream = await navigator.mediaDevices.getUserMedia(
@@ -19,6 +38,7 @@ async function getMedia() {
         );
 
         myFace.srcObject = myStream;
+        await getCameras();
     } catch (e) {
         console.error(e.message);
     }
@@ -26,7 +46,10 @@ async function getMedia() {
 
 getMedia();
 
-const handleMuteBtnClick = () => {
+const handleMuteBtnClick = () => {        
+
+    myStream.getAudioTracks().forEach(track => track.enabled = !track.enabled);
+
     if ( ! muted) {
         muteBtn.innerHTML = 'Unmute';
     } else {
@@ -35,12 +58,15 @@ const handleMuteBtnClick = () => {
 
     muted = ! muted;
 }
-const handleTurnCameraBtnClick = () => {
+const handleTurnCameraBtnClick = () => {    
+
+    myStream.getVideoTracks().forEach(track => track.enabled = !track.enabled);
+
     if ( ! cameraOff) {
         cameraBtn.innerHTML = 'Turn Camera Off';    
     } else {
         cameraBtn.innerHTML = 'Turn Camera On';
-    }
+    }    
 
     cameraOff = ! cameraOff;
 }
